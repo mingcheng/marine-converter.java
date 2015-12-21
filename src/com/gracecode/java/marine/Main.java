7e796aaca48e18ba2e73d55aa5615881fbbb1522
明城 <lucky@gracecode.com>
2015-12-21 16:26:38 +0800

    使用命令行传入参数

--- src/com/gracecode/java/marine/Main.java
+++ src/com/gracecode/java/marine/Main.java
@@ -1,5 +1,7 @@
 package com.gracecode.java.marine;
 
+import org.apache.commons.cli.*;
+
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
@@ -7,11 +9,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
-    private static final String SOURCE_DIR = "/Users/mingcheng/Desktop/NMEA/";
-    private static final String OUTPUT_DIR = "/Users/mingcheng/Desktop/json/";
+    //    private static final String SOURCE_DIR = "/Users/mingcheng/Desktop/NMEA/";
+//    private static final String OUTPUT_DIR = "/Users/mingcheng/Desktop/json/";
     private static final int THREAD_NUM = 10;
-
-    private static ExecutorService threadPool;
+    private static File NMEADirectory;
+    private static File JSONDirectory;
 
     private static class ConverterRunnable implements Runnable {
         private final SentenceConverter converter;
@@ -27,28 +29,55 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        threadPool = Executors.newFixedThreadPool(THREAD_NUM);
+        ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_NUM);
 
-        File NMEADirectory = new File(SOURCE_DIR);
-        File[] NMEAFiles = NMEADirectory.listFiles(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.endsWith(".LOG");
-            }
-        });
+        Options options = new Options();
+        options.addOption("i", "input-dir", true, "input directory");
+        options.addOption("o", "output-dir", true, "output directory");
+        options.addOption("f", "focus", false, "focus to create/overwrite file");
 
-        for (File NMEAFile : NMEAFiles) {
-            File JSONFile = getJSONFileFromNMEAFile(NMEAFile);
+        try {
+            CommandLine commandLine = new DefaultParser().parse(options, args);
+            boolean focus = commandLine.hasOption("f");
             try {
-                threadPool.execute(new ConverterRunnable(NMEAFile, JSONFile));
-            } catch (IOException e) {
-                e.printStackTrace();
+                NMEADirectory = new File(commandLine.getOptionValue("i"));
+                JSONDirectory = new File(commandLine.getOptionValue("o"));
+            } catch (NullPointerException e) {
+                throw new ParseException("");
             }
+
+            if (!NMEADirectory.canRead() || !NMEADirectory.isDirectory()) {
+                throw new IOException(NMEADirectory.getAbsolutePath() + " is NOT readable.");
+            }
+
+            if (!JSONDirectory.canWrite() || !JSONDirectory.isDirectory()) {
+                throw new IOException(JSONDirectory.getAbsolutePath() + " is NOT writable.");
+            }
+
+            File[] NMEAFiles = NMEADirectory.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.endsWith(".LOG");
+                }
+            });
+
+            for (File NMEAFile : NMEAFiles) {
+                File JSONFile = getJSONFileFromNMEAFile(NMEAFile);
+                try {
+                    threadPool.execute(new ConverterRunnable(NMEAFile, JSONFile));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (ParseException e) {
+            new HelpFormatter().printHelp(Main.class.getName(), options);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 
     private static File getJSONFileFromNMEAFile(File NMEAFile) {
         String basename = NMEAFile.getName().replaceAll("[.][^.]+$", "");
-        return new File(OUTPUT_DIR + basename + ".json");
+        return new File(JSONDirectory.getAbsolutePath() + File.pathSeparator + basename + ".json");
     }
 }
